7f3b5b484f046b64962cc72ea02c83318fc0c24c
Forrest Walker <forrestpatwalker@hotmail.com>
2024-02-24 16:05:23 -0700

    added sprite to scene and added logic to move in circles

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use godot::prelude::*;
+use godot::engine::{Sprite2D, ISprite2D};
+
+struct MyExtension;
+
+#[gdextension]
+unsafe impl ExtensionLibrary for MyExtension {}
+
+#[derive(GodotClass)]
+#[class(base=Sprite2D)]
+struct Player {
+    speed: f64,
+    angular_speed: f64,
+    base: Base<Sprite2D>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[godot_api]
+impl ISprite2D for Player {
+    fn init(base: Base<Sprite2D>) -> Self {
+        godot_print!("Hello, world!"); // Prints to the Godot console
+        
+        Self {
+            speed: 400.0,
+            angular_speed: std::f64::consts::PI,
+            base,
+        }
     }
+
+    fn physics_process(&mut self, delta: f64) {
+        let radians = (self.angular_speed * delta) as f32;
+        self.base_mut().rotate(radians);
+
+        let rotation = self.base().get_rotation();
+        let velocity = Vector2::UP.rotated(rotation) * self.speed as f32;
+        self.base_mut().translate(velocity * delta as f32);
+    }
+}
+
+#[godot_api]
+impl Player {
+    #[func]
+    fn increase_speed(&mut self, amount: f64) {
+        self.speed += amount;
+        self.base_mut().emit_signal("speed_increased".into(), &[]);
+    }
+
+    #[signal]
+    fn speed_increased();
 }
